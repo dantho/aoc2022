6232af0a33f6a854dd6dd04073dd880b5ee7b824
Dan T <dan.thornton+Git@thornton.me>
2022-12-02 08:50:16 -0500

    day 2 part 2 done

--- src/day2.rs
+++ src/day2.rs
@@ -14,14 +14,14 @@ use std::str::FromStr;
 // ********************/
 #[derive(Clone, Copy, Debug)]
 pub enum RockPaperScissors {
-    Rock = 1,
+    Rock = 0,
     Paper,
     Scissors
 }
 
 impl RockPaperScissors {
     fn score(self, vs: RockPaperScissors) -> u32 {
-        self as u32 + match self {
+        1 + self as u32 + match self {
             Self::Rock => match vs {
                 Self::Rock => 3,
                 Self::Paper => 0,
@@ -40,7 +40,16 @@ impl RockPaperScissors {
         }
     }
 }
-
+impl From<u32> for RockPaperScissors {
+    fn from(v: u32) -> Self {
+        match v {
+            n if n == Self::Rock as u32 => Self::Rock,
+            n if n == Self::Paper as u32 => Self::Paper,
+            n if n == Self::Scissors as u32 => Self::Scissors,
+            _ => panic!("Expecting value 0, 1, or 2")
+        }
+    }
+}
 impl ToString for RockPaperScissors {
     fn to_string(&self) -> String {
         match self {
@@ -63,7 +72,7 @@ impl FromStr for RockPaperScissors {
     }
 }
 
-#[aoc_generator(day2)]
+#[aoc_generator(day2, part1)]
 pub fn gen1(input: &str) -> Vec<(RockPaperScissors,RockPaperScissors)> {
     input.lines()
     .map(|line| {
@@ -72,11 +81,38 @@ pub fn gen1(input: &str) -> Vec<(RockPaperScissors,RockPaperScissors)> {
     }).collect()
 }
 
+#[aoc_generator(day2, part2)]
+pub fn gen2(input: &str) -> Vec<(RockPaperScissors,RockPaperScissors)> {
+    input.lines()
+    .map(|line| {
+        let mut play = line.split(' ');
+        let vs = play.next().unwrap().parse::<RockPaperScissors>().unwrap();
+        let me = match play.next().unwrap() {
+            "X" => RockPaperScissors::from((vs as u32 + 2) % 3),
+            "Y" => vs,
+            "Z" => RockPaperScissors::from((vs as u32 + 1) % 3),
+            bad => panic!("Unexpected instructional play \"{}\"", bad)
+            // "X means you need to lose,
+            //  Y means you need to end the round in a draw,
+            //  and Z means you need to win."
+        };
+        (vs, me)
+    }).collect()
+}
+
 // *********************
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day2, part1)]
 pub fn part1(input: &[(RockPaperScissors,RockPaperScissors)]) -> u32 {
+    let ans = input.iter().map(|pair|pair.1.score(pair.0)).sum();
+    #[cfg(not(test))]
+    assert!(ans > 11333);
+    ans
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &[(RockPaperScissors,RockPaperScissors)]) -> u32 {
     let ans = input.iter().map(|pair|pair.1.score(pair.0)).sum();
     #[cfg(not(test))]
     assert!(ans > 11333);
@@ -93,6 +129,11 @@ mod tests {
     #[test]
     fn test_ex1_part1() {
         assert_eq!(part1(&gen1(EX1)), 15);
+    }
+
+    #[test]
+    fn test_ex1_part2() {
+        assert_eq!(part2(&gen2(EX1)), 12);
     }
 
 const EX1: &'static str =
