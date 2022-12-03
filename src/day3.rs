e9508a96903c70d300329d708720eafaef0dcbd7
Dan T <dan.thornton+Git@thornton.me>
2022-12-03 05:03:10 -0500

    day 3 part 2 done

--- src/day3.rs
+++ src/day3.rs
@@ -7,12 +7,12 @@
 // extern crate regex;
 // use self::regex::{Captures, Regex};
 
-use std::{str::FromStr, collections::HashMap};
+use std::collections::HashMap;
 
 // ********************
 // *** Generator(s) ***
 // ********************/
-#[aoc_generator(day3, part1)]
+#[aoc_generator(day3)]
 pub fn gen1(input: &str) -> Vec<String> {
     input.lines().map(|s|s.to_owned()).collect()
 }
@@ -32,11 +32,40 @@ pub fn part1(input: &[String]) -> u32 {
         let mut found: char = '-';
         for c in sac1.chars() {
             if sac2.contains(c) {
+                if found != '-' && found != c {
+                    panic!("Found '{}' after '{}'", c, found);
+                }
                 found = c;
-                break;
             }
         }
         assert_ne!(found, '-');
+        // println!("{} ({})", priority[&found], found);
+        priority[&found]
+    }).sum()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &[String]) -> u32 {
+    let priority = ('a'..='z').chain('A'..='Z').zip(1..).collect::<HashMap<char,u32>>();
+    input.iter().step_by(3).zip(
+        input.iter().skip(1).step_by(3).zip(
+            input.iter().skip(2).step_by(3)))
+    .map(|(e1, (e2, e3))|{
+        let mut found: char = '-';
+        let mut maybe = String::new();
+        for c in e1.chars() {
+            if e2.contains(c) {maybe.push(c)};
+        }
+        for c in maybe.chars() {
+            if e3.contains(c) {
+                if found != '-' && found != c {
+                    panic!("Found '{}' after '{}'", c, found);
+                }
+                found = c;
+            }
+        }
+        assert_ne!(found, '-');
+        // println!("{} ({})", priority[&found], found);
         priority[&found]
     }).sum()
 }
@@ -50,17 +79,19 @@ mod tests {
 
     #[test]
     fn test_ex1_part1() {
-        assert_eq!(part1(&gen1(EX1)), 15);
+        assert_eq!(part1(&gen1(EX1)), 157);
     }
 
     #[test]
     fn test_ex1_part2() {
-        assert_eq!(part2(&gen2(EX1)), 12);
+        assert_eq!(part2(&gen1(EX1)), 70);
     }
 
 const EX1: &'static str =
-r"A Y
-B X
-C Z";
-
+r"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 }
