57967ed7405197b74ee18be1768806fc0ea010cf
Dan T <dan.thornton+Git@thornton.me>
2022-12-19 07:47:01 -0500

    add parse_numbers() with tests

--- src/day19.rs
+++ src/day19.rs
@@ -11,44 +11,32 @@
 // *** Generator(s) ***
 // ********************/
 #[aoc_generator(day19)]
-pub fn gen1(input: &str) -> Vec<Vec<u32>> {
-    let mut elf = Vec::<u32>::new();
-    let mut elves = Vec::new();
-    for line in input.lines() {
-        if line == "" {
-            elves.push(elf);
-            elf = Vec::new();
-        } else {
-            elf.push(line.parse().unwrap());
-        }
-    }
-    elves.push(elf);
-    elves
+pub fn gen1(input: &str) -> Vec<Vec<isize>> {
+    input.lines()
+        .map(|line| parse_numbers(line))
+        .collect::<Vec<_>>()
 }
 
 // *********************
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day19, part1)]
-pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
-    input
-        .iter()
-        .map(|elf| elf.iter().fold(0, |sum, item| sum + item))
-        .max()
-        .unwrap()
+pub fn part1(input: &[Vec<isize>]) -> usize {
+    input.len() * input[0].len()
 }
 
-#[aoc(day19, part2)]
-pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
-    let mut calories: Vec<u32> = input
-        .iter()
-        .map(|elf| elf.iter().fold(0, |sum, item| sum + item))
-        .collect();
-    calories.sort();
-    calories.reverse();
-    calories.iter().take(3).sum()
-}
+// #[aoc(day19, part2)]
+// pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
+//     999
+// }
 
+fn parse_numbers(s: &str) -> Vec<isize> {
+    let just_nums: String = s.chars()
+    .map(|c| if c.is_numeric() || c == '-' {c} else {' '}).collect();
+    just_nums.trim().split(' ')
+        .filter(|s|!s.is_empty())
+        .map(|s|s.parse().unwrap()).collect()
+}
 // *************
 // *** Tests ***
 // *************
@@ -57,27 +45,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_numbers() {
+        assert_eq!(parse_numbers("xyz123abc456def"),[123,456]);
+        assert_eq!(parse_numbers("xyz123abc-456def"),[123,-456]);
+        assert_eq!(parse_numbers("123 456"),[123,456]);
+        assert_eq!(parse_numbers("&123:-456#"),[123,-456]);
+
+        assert_eq!(parse_numbers("Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian."),
+            [1,4,2,3,14,2,7]);
+
+        assert_eq!(parse_numbers("humanoid robot"),[]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_parse_numbers_solitarydash() {
+        // Not yet implemented
+        assert_eq!(parse_numbers("humanoid-robot"),[]);
+    }
+    #[test]
     fn test_ex1_part1() {
         assert_eq!(part1(&gen1(EX1)), 24000);
     }
 
-    #[test]
-    fn test_ex1_part2() {
-        assert_eq!(part2(&gen1(EX1)), 45000);
-    }
+    // #[test]
+    // fn test_ex1_part2() {
+    //     assert_eq!(part2(&gen1(EX1)), 45000);
+    // }
 
-    const EX1: &'static str = r"1000
-2000
-3000
+    const EX1: &'static str = 
+r"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian. 
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
 
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
 }
