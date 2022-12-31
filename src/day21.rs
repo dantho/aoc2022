0283804dffb1debbca3e3a2c5212815c27391207
Dan Thornton <dan.thornton+Git@thornton.me>
2022-12-31 15:28:19 -0500

    day 23 done

--- src/day21.rs
+++ src/day21.rs
@@ -16,6 +16,45 @@ pub struct Monkey {
     math: Ops,
 }
 
+impl Monkey {
+    fn swap(self, with_first: bool) -> Monkey {
+        let Monkey { name, known, monkeys, math} = self;
+        if let Some((m1,m2)) = monkeys {
+            Monkey {
+                name: if with_first {m1.clone()} else {m2.clone()},
+                known,
+                monkeys: Some(
+                    if with_first {
+                        (name, m2)
+                    } else {
+                        if math == Sub || math == Div {
+                            (m1, name)
+                        } else {
+                            (name, m1)
+                        }
+                    }),
+                math: match math {
+                    Add => Sub,
+                    Sub => if with_first {
+                            Add
+                        } else {
+                            Sub
+                        },
+                    Mul => Div,
+                    Div => if with_first {
+                            Mul
+                        } else {
+                            Div
+                        },
+                Known => panic!("swap not supported for Known monkeys"),
+                }
+            }
+        } else {
+            panic!("Can't swap a Known monkey!");
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Ops {
     Add,
@@ -56,7 +95,7 @@ pub fn gen1(input: &str) -> Vec<Monkey> {
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day21, part1)]
-pub fn part1(input: &[Monkey]) -> u64 {
+pub fn part1(input: &[Monkey]) -> Option<u64> {
     let monkeys = input.to_vec();
     let mut known_monkeys = monkeys.iter().filter(|m|m.known.is_some())
         .map(|m| (m.name.to_string(), m.clone()))
@@ -96,7 +135,139 @@ pub fn part1(input: &[Monkey]) -> u64 {
         }).collect();
     }
     
-    known_monkeys["root"].known.unwrap()
+    known_monkeys["root"].known
+
+}
+
+#[aoc(day21, part2)]
+pub fn part2(input: &[Monkey]) -> Option<u64> {
+    let monkeys = input.to_vec();
+    let mut known_monkeys = monkeys.iter().filter(|m|m.known.is_some())
+        .map(|m| (m.name.to_string(), m.clone()))
+        .collect::<HashMap<_, _>>();
+    known_monkeys.remove("humn");
+    let mut unknown_monkeys = monkeys.into_iter()
+        .filter(|m| m.known.is_none()).collect::<Vec<_>>();
+    let mut last_len = usize::MIN;
+    while known_monkeys.len() > last_len {
+        last_len = known_monkeys.len();
+        for unk_monk in &mut unknown_monkeys {
+            if let Some((m1, m2)) = &unk_monk.monkeys {
+                if known_monkeys.contains_key(m1) && known_monkeys.contains_key(m2) {
+                    if let Some(n1) = known_monkeys[m1].known {
+                        if let Some(n2) = known_monkeys[m2].known {
+                            let now_known = match &unk_monk.math {
+                                Add => Some(n1 + n2),
+                                Sub => {
+                                    if n1 >= n2 {
+                                        Some(n1 - n2)
+                                    } else {
+                                        Some(0)
+                                    }
+                                },
+                                Mul => Some(n1 * n2),
+                                Div => Some(n1 / n2),
+                                Known => panic!("Should not be known already"),
+                            };
+                            unk_monk.known = now_known;
+                            unk_monk.monkeys = None;
+                        }
+                    }
+                }
+            } else {
+                panic!("Unknown monkeys MUST have .monkeys defined");
+            }
+        }
+        unknown_monkeys = unknown_monkeys.into_iter().filter_map(|m| {
+            if m.known.is_some() {
+                known_monkeys.insert(m.name.to_string(), m);
+                None
+            } else {
+                Some(m)
+            }
+        }).collect();
+    }
+
+    // Root's math is a wierd == in Part 2.  This translates into subtracting equals...
+    unknown_monkeys = unknown_monkeys.into_iter().map(|mut m| {
+        if m.name == "root" {
+            m.math = Sub;
+        }
+        m
+    }).collect();
+
+    // ...yields 0.
+    known_monkeys.insert("root".to_string(),
+        Monkey {
+            name: "root".to_string(),
+            known: Some(0),
+            monkeys: None,
+            math: Known,
+        });
+
+    // This is the heart of Part 2's solution -- rejiggering equations via .swap().
+    unknown_monkeys = unknown_monkeys.into_iter().map(|m| {
+        if let Some((m1,m2)) = &m.monkeys {
+            match (known_monkeys.contains_key(m1), known_monkeys.contains_key(m2)) {
+                (false, false) => m,
+                (false, true) => m.swap(true),
+                (true, false) => m.swap(false),
+                (true, true) => panic!("Found a known monkey in the unknowns."),
+            }
+        } else {
+            panic!("Can't happen.")
+        }
+    }).collect();
+
+    // Finally, continue as in Part 1
+    while !unknown_monkeys.is_empty() {
+        for unk_monk in &mut unknown_monkeys {
+            if let Some((m1, m2)) = &unk_monk.monkeys {
+                if known_monkeys.contains_key(m1) && known_monkeys.contains_key(m2) {
+                    if let Some(n1) = known_monkeys[m1].known {
+                        if let Some(n2) = known_monkeys[m2].known {
+                            let now_known = match &unk_monk.math {
+                                Add => Some(n1 + n2),
+                                Sub => {
+                                    if n1 >= n2 {
+                                        Some(n1 - n2)
+                                    } else {
+                                        Some(0)
+                                    }
+                                },
+                                Mul => Some(n1 * n2),
+                                Div => Some(n1 / n2),
+                                Known => panic!("Should not be known already"),
+                            };
+                            unk_monk.known = now_known;
+                            unk_monk.monkeys = None;
+                        }
+                    }
+                }
+            } else {
+                panic!("Unknown monkeys MUST have .monkeys defined");
+            }
+        }
+        unknown_monkeys = unknown_monkeys.into_iter().filter_map(|m| {
+            if m.known.is_some() {
+                known_monkeys.insert(m.name.to_string(), m);
+                None
+            } else {
+                Some(m)
+            }
+        }).collect();
+    }
+
+    let partially_known = unknown_monkeys.iter().filter(|m| {
+        if let Some((m1,m2)) = &m.monkeys {
+            known_monkeys.contains_key(m1) || known_monkeys.contains_key(m2)
+        } else {
+            false
+        }
+    }).count();
+    println!("{} unknowns have {} that contain a known.", unknown_monkeys.len(), partially_known);
+
+    known_monkeys["humn"].known
 
 }
 
@@ -109,13 +280,13 @@ mod tests {
 
     #[test]
     fn test_ex1_part1() {
-        assert_eq!(part1(&gen1(EX1)), 152);
+        assert_eq!(part1(&gen1(EX1)), Some(152));
     }
 
-    // #[test]
-    // fn test_ex1_part2() {
-    //     assert_eq!(part2(&true), 2713310158);
-    // }
+    #[test]
+    fn test_ex1_part2() {
+        assert_eq!(part2(&gen1(EX1)), Some(301));
+    }
 
     #[allow(unused)]
     const EX1: &'static str = r"root: pppw + sjmn
--- src/day23.rs
+++ src/day23.rs
@@ -1,23 +1,34 @@
+use std::collections::{HashSet, HashMap};
+
 /// https://adventofcode.com/2022/day/23
 /// DAN AoC: https://adventofcode.com/2022/leaderboard/private/view/380786
 /// HLOTYAK: https://adventofcode.com/2022/leaderboard/private/view/951754
+
+use crate::day23::Dirs::*;
+
+pub enum Dirs {
+    North,
+    South,
+    West,
+    East
+}
 
 // ********************
 // *** Generator(s) ***
 // ********************/
 #[aoc_generator(day23)]
-pub fn gen1(input: &str) -> Vec<Vec<u32>> {
-    let mut elf = Vec::<u32>::new();
+pub fn gen1(input: &str) -> Vec<(usize,usize)> {
+    const BORDER: usize = 100;
     let mut elves = Vec::new();
-    for line in input.lines() {
-        if line == "" {
-            elves.push(elf);
-            elf = Vec::new();
-        } else {
-            elf.push(line.parse().unwrap());
+    let lines = input.lines().collect::<Vec<_>>();
+    for y in 0..lines.len() {
+        let chars = lines[y].chars().collect::<Vec<_>>();
+        for x in 0..lines[y].len() {
+            if '#' == chars[x] {
+                elves.push((BORDER + y, BORDER + x));
+            }
         }
     }
-    elves.push(elf);
     elves
 }
 
@@ -25,25 +36,89 @@ pub fn gen1(input: &str) -> Vec<Vec<u32>> {
 // *** Part1 & Part2 ***
 // *********************
 #[aoc(day23, part1)]
-pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
-    input
-        .iter()
-        .map(|elf| elf.iter().fold(0, |sum, item| sum + item))
-        .max()
-        .unwrap()
+pub fn part1(input: &[(usize,usize)]) -> usize {
+    let look_dirs = [North, South, West, East];
+    let mut elves = input.iter().cloned().collect::<HashSet<_>>();
+    let mut move_count = 0;
+    #[cfg(test)]
+    print_elves(&elves);
+    let mut crowded = elves.iter()
+        .filter_map(|&(y,x)| {
+            if [(y+1,x-1),(y+1,x),(y+1,x+1),(y-1,x-1),(y-1,x),(y-1,x+1),(y,x-1),(y,x+1)].iter()
+            .fold(false, |nearby_detected, pos| nearby_detected || elves.contains(pos)) {
+                Some((y,x))
+            } else {
+                None
+            }}).collect::<HashSet<_>>();
+    while !crowded.is_empty() {
+        let mut proposed: HashMap<(usize,usize), Option<(usize,usize)>> = HashMap::new();
+        for &(y,x) in &crowded {
+            for look in move_count..move_count+4 {
+                let search = match look_dirs[look % 4] {
+                    North => [(y-1,x-1),(y-1,x),(y-1,x+1)],
+                    South => [(y+1,x-1),(y+1,x),(y+1,x+1)],
+                    West =>  [(y-1,x-1),(y,x-1),(y+1,x-1)],
+                    East =>  [(y-1,x+1),(y,x+1),(y+1,x+1)],
+                };
+                let available = !search.iter().fold(false, |occupied, pos| occupied || elves.contains(pos));
+                if available {
+                    // None indicates conflict
+                    proposed.entry(search[1]).and_modify(|v| *v = None).or_insert(Some((y,x)));
+                    break;
+                }
+            }
+        }
+        proposed.into_iter().for_each(|(to, maybe_from)|
+            if let Some(from) = maybe_from {
+                // println!("Moving from {:?} to {:?}",from,to);
+                elves.remove(&from);
+                elves.insert(to);
+            }
+        );
+        crowded = elves.iter().filter_map(|&(y,x)| {
+            if [(y+1,x-1),(y+1,x),(y+1,x+1),(y-1,x-1),(y-1,x),(y-1,x+1),(y,x-1),(y,x+1)].iter()
+            .fold(false, |nearby_detected, pos| nearby_detected || elves.contains(pos)) {
+                Some((y,x))
+            } else {
+                None
+            }}).collect::<HashSet<_>>();
+
+            #[cfg(test)]
+            print_elves(&elves);
+
+            move_count += 1;
+        // Part 1 only
+        // if move_count == 10 {
+        //     crowded = HashSet::new(); // terminate loop
+        // }
+    }
+    // Part 1
+    // let (ymin,ymax,xmin,xmax) = elven_boundaries(&elves);
+    // (ymax-ymin+1)*(xmax-xmin+1)-elves.len()
+
+    // Part 2
+    move_count + 1
+
 }
 
-#[aoc(day23, part2)]
-pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
-    let mut calories: Vec<u32> = input
-        .iter()
-        .map(|elf| elf.iter().fold(0, |sum, item| sum + item))
-        .collect();
-    calories.sort();
-    calories.reverse();
-    calories.iter().take(3).sum()
+fn elven_boundaries(elves: &HashSet<(usize,usize)>) -> (usize, usize, usize, usize) {
+    elves.iter()
+    .fold((usize::MAX,0,usize::MAX,0), |(ymin,ymax,xmin,xmax), &(y,x) | (
+        y.min(ymin),
+        y.max(ymax),
+        x.min(xmin),
+        x.max(xmax)))
 }
 
+fn print_elves(elves: &HashSet<(usize,usize)>) {
+    let (ymin,ymax,xmin,xmax) = elven_boundaries(&elves);
+    for y in ymin..=ymax {
+        for x in xmin..=xmax {
+            print!("{}", if elves.contains(&(y,x)) {'#'} else {'.'});
+        }
+        println!();
+    }
+}
 // *************
 // *** Tests ***
 // *************
@@ -53,26 +128,34 @@ mod tests {
 
     #[test]
     fn test_ex1_part1() {
-        assert_eq!(part1(&gen1(EX1)), 24000);
+        assert_eq!(part1(&gen1(EX1)), 110);
     }
 
     #[test]
-    fn test_ex1_part2() {
-        assert_eq!(part2(&gen1(EX1)), 45000);
+    fn test_ex2_part1() {
+        assert_eq!(part1(&gen1(EX2)), 25);
     }
 
-    const EX1: &'static str = r"1000
-2000
-3000
+    // #[test]
+    // fn test_ex1_part2() {
+    //     assert_eq!(part2(&gen1(EX1)), 45000);
+    // }
 
-4000
+    const EX1: &'static str =
+r"....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..";
 
-5000
-6000
+const EX2: &'static str =
+r".....
+..##.
+..#..
+.....
+..##.
+.....";
 
-7000
-8000
-9000
-
-10000";
 }
